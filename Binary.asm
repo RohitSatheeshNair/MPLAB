348c041d3c21e2c8333352725950b117a282139c
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2020-10-28 14:04:16 +0530

    Update Binary.asm

--- Binary.asm
+++ Binary.asm
@@ -1,51 +1,54 @@
-; PROGRAM :: ASSEMBLY LANGUAGE PROGRAM TO SEARCH A KEY ELEMENT IN A
-;            LIST OF 'n' NUMBER USING THE BINARY SEARCH ALGORITHM
+.model small
+display macro msg
+        lea dx,msg
+        mov ah,09h
+        int 21h
+endm
 
-.MODEL SMALL
+.data
+list db 01h,05h,07h,10h,12h,14h
+number equ($-list)
+key db 10h
+msg1 db 0dh,0ah,"element found in list..$"
+msg2 db 0dh,0ah,"search failed!!element not found $"
 
-; MACRO TO DISPLAY THE MESSAGE....
-DISPLAY MACRO MSG
-        LEA DX, MSG
-        MOV AH, 09H
-        INT 21H
-ENDM
+.code
+start:mov ax,@data
+      mov ds,ax
 
-.DATA
-LIST DB 01H, 05H, 07H, 10H, 12H, 14H
-NUMBER EQU ($-LIST)
-KEY DB 011H             
-MSG1 DB 0DH, 0AH, "ELEMENT FOUND IN THE LIST...$"
-MSG2 DB 0DH, 0AH, "SEARCH FAILED !! ELEMENT NOT FOUND IN THE LIST $"
+      mov ch,number-1
+      mov cl,00h
 
-.CODE
-START : MOV AX, @DATA
-        MOV DS, AX
-        MOV CH, NUMBER-1   ; HIGH VALUE...
-        MOV CL, 00H        ; LOW VALUE...
-AGAIN:  MOV SI, OFFSET LIST
-        XOR AX, AX
-        CMP CL, CH
-        JE NEXT
-        JNC FAILED
-NEXT:   MOV AL, CL
-        ADD AL, CH
-        SHR AL, 01H             ; DIVIDE BY 2
-        MOV BL, AL
-        XOR AH, AH              ; CLEAR AH
-        MOV BP, AX
-        MOV AL, DS:[BP][SI]
-        CMP AL, KEY             ; COMPARE KEY AND A[i]        
-        JE SUCCESS              ; IF EQUAL, DISPLAY SUCCESS MESSAGE
-        JC INCLOW
-        MOV CH, BL              ; IF KEY>A[i]  SHIFT HIGH        
-        DEC CH
-        JMP AGAIN
-INCLOW: MOV CL, BL              ; IF KEY<A[i]  SHIFT LOW
-        INC CL
-        JMP AGAIN
-SUCCESS:DISPLAY MSG1            
-        JMP FINAL
-FAILED: DISPLAY MSG2            ; JOB OVER. TERMINATE....
-FINAL : MOV AH, 4CH
-        INT 21H
-END START
+again: mov si,offset list
+       xor ax,ax
+       cmp cl,ch
+       je next
+       jnc failed
+
+next: mov al,cl
+      add al,ch
+      shr al,01h
+
+      mov bl,al
+      xor ah,ah
+      mov bp,ax
+      mov al,ds:[bp][si]
+      cmp al,key
+      je success
+      jc inclow
+      mov ch,bl
+      dec ch
+      jmp again
+
+inclow: mov cl,bl
+        inc cl
+        jmp again
+
+success: display msg1
+         jmp final
+
+failed:display msg2
+
+final:mov ah,4ch
+       int 21h
+end start
